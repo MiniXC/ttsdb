731a1f2675360e1b122a5e31db8404dabf0dd4ff
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2024-06-12 17:10:35 +0100

    switch back to pyannote

--- src/ttsdb/benchmarks/speaker/wespeaker.py
+++ src/ttsdb/benchmarks/speaker/wespeaker.py
@@ -6,6 +6,8 @@ from tqdm import tqdm
 import numpy as np
 import librosa
 import soundfile as sf
+from pyannote.audio import Model, Inference
+
 
 from ttsdb.benchmarks.benchmark import Benchmark, BenchmarkCategory, BenchmarkDimension
 from ttsdb.util.dataset import Dataset
@@ -25,7 +27,8 @@ class WeSpeakerBenchmark(Benchmark):
             dimension=BenchmarkDimension.N_DIMENSIONAL,
             description="The speaker embeddings using WeSpeaker.",
         )
-        self.model = wespeaker.load_model('english')
+        self.model = Model.from_pretrained("pyannote/wespeaker-voxceleb-resnet34-LM")
+        self.inference = Inference(self.model, window="sliding", duration=3.0, step=1.0)
 
     def _get_distribution(self, dataset: Dataset) -> np.ndarray:
         """
@@ -45,7 +48,49 @@ class WeSpeakerBenchmark(Benchmark):
                 )
             with tempfile.NamedTemporaryFile(suffix=".wav") as f:
                 sf.write(f.name, wav, 16000)
-                embedding = self.model.extract_embedding(f.name).numpy()
-            embeddings.append(embedding)
+                embedding = self.inference(f.name)
+                embedding = [x[1] for x in embedding]
+            embeddings.extend(embedding)
+        [print(x.shape) for x in embeddings]
         embeddings = np.vstack(embeddings)
+        print(embeddings.shape)
         return embeddings
+
+# class WeSpeakerBenchmark(Benchmark):
+#     """
+#     Benchmark class for the WeSpeaker benchmark.
+#     """
+
+#     def __init__(
+#         self,
+#     ):
+#         super().__init__(
+#             name="WeSpeaker",
+#             category=BenchmarkCategory.SPEAKER,
+#             dimension=BenchmarkDimension.N_DIMENSIONAL,
+#             description="The speaker embeddings using WeSpeaker.",
+#         )
+#         self.model = wespeaker.load_model('english')
+
+#     def _get_distribution(self, dataset: Dataset) -> np.ndarray:
+#         """
+#         Get the distribution of the WeSpeaker benchmark.
+
+#         Args:
+#             dataset (Dataset): The dataset to get the distribution from.
+
+#         Returns:
+#             np.ndarray: The distribution of the WeSpeaker benchmark.
+#         """
+#         embeddings = []
+#         for wav, _, _ in tqdm(dataset, desc=f"computing embeddings for {self.name}"):
+#             if dataset.sample_rate != 16000:
+#                 wav = librosa.resample(
+#                     wav, orig_sr=dataset.sample_rate, target_sr=16000
+#                 )
+#             with tempfile.NamedTemporaryFile(suffix=".wav") as f:
+#                 sf.write(f.name, wav, 16000)
+#                 embedding = self.model.extract_embedding(f.name).numpy()
+#             embeddings.append(embedding)
+#         embeddings = np.vstack(embeddings)
+#         return embeddings
